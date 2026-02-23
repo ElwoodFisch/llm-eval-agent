1b6e738d9cf6ebf9db6549238325a2542ab38b84
ElwoodFisch <lelwoodfischer@gmail.com>
2026-02-23 12:11:13 -0600

    Feature: batch evaluation with 3 test cases (list & loop)

--- evaluator.py
+++ evaluator.py
@@ -5,34 +5,41 @@ from dotenv import load_dotenv
 load_dotenv()
 client = OpenAI()
 
-#INPUT TEST CASE
-prompt = "Explain how they can resurrect dinosaurs, in one sentence"
+#INPUT LIST OF TEST CASES
+prompts = [
+    "Explain artificial intelligence in one sentence.",
+    "Explain machine lerning in one sentence.",
+    "Explain neural networks in one sentence."
+]
 
-#MODEL RESPONSE
-response = client.chat.completions.create(
-    model="gpt-4.1-mini",
-    messages=[
-        {"role": "user", "content": prompt}
-    ]
-)
+#LOOP to collect MODEL RESPONSES for each case
+for prompt in prompts:
+    response = client.chat.completions.create(
+        model="gpt-4.1-mini",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
 
-#RUN TEST
-output = response.choices[0].message.content
-word_count = len(output.split())
-if word_count > 25:
-    result = "FAIL: Too long"
-else: 
-    result = "PASS"    
+#RUN TEST CASES
+    output = response.choices[0].message.content
+
+    word_count = len(output.split())
+    
+    if word_count > 25:
+        result = "FAIL: Too long"
+    else: 
+        result = "PASS"    
 
 #TEST OUTPUT
-print("PROMPT:")
-print(prompt)
+    print("\n--- Test Case ---")
+    print("PROMPT:")
+    print(prompt)
 
-print("\nMODEL OUTPUT:")
-print(output)
+    print("\nMODEL OUTPUT:")
+    print(output)
 
-
-print("\nEVALUATION")
-print(result)
-print(f"Word count: {word_count}")
+    print("\nEVALUATION")
+    print(result)
+    print(f"Word count: {word_count}")
 
